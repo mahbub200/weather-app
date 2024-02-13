e05049a69c1467e90f5c63fb4df3e27626a45ebd
Mahbub Faruque Ahmed <mahbubf200@gmail.com>
2024-02-13 18:45:22 +0600

    favourite context added

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,21 @@ import "./App.css";
 import Header from "./components/header/Header";
 import WeatherBoard from "./components/weather/WeatherBoard";
 import { WeatherProvider } from "./provider";
+import FavouriteProvider from "./provider/FavouriteProvider";
 
 function App() {
   return (
     <WeatherProvider>
-      <div className="h-screen grid place-items-center">
-        <Header />
-        <main>
-          <section className="">
-            <WeatherBoard></WeatherBoard>
-          </section>
-        </main>
-      </div>
+      <FavouriteProvider>
+        <div className="h-screen grid place-items-center">
+          <Header />
+          <main>
+            <section className="">
+              <WeatherBoard></WeatherBoard>
+            </section>
+          </main>
+        </div>
+      </FavouriteProvider>
     </WeatherProvider>
   );
 }
